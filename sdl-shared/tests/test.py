74d8e2e6a4dd5315bedb9963bc8f5bdff9aba80f
AllPlas <proprikol987@gmail.com>
2023-04-10 00:00:34 +0300

    added tests

---
+++ sdl-shared/tests/test.py
@@ -0,0 +1,12 @@
+import sys
+
+
+def test_sdl_shared():
+    line = sys.stdin.readline().strip()
+    version = line.split(":")[1]
+    major_version = int(version.split(".")[0])
+    assert major_version >= 3
+
+
+if __name__ == '__main__':
+    test_sdl_shared()
