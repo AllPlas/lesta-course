cc36b1f3bc8ddf2b4e9493ea41e7b94408f7811d
AllPlas <proprikol987@gmail.com>
2023-04-10 00:35:06 +0300

    update tests

--- sdl-static/tests/test.py
+++ sdl-static/tests/test.py
@@ -5,7 +5,7 @@ def test_sdl_static():
     line = sys.stdin.readline().strip()
     version = line.split(":")[1]
     major_version = int(version.split(".")[0])
-    assert major_version >= 4
+    assert major_version >= 3
 
 
 if __name__ == '__main__':
