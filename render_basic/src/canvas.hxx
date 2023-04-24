8cf4284e88c11a4da0af5f7263c6bc56b7df5c40
AllPlas <proprikol987@gmail.com>
2023-04-25 02:00:36 +0300

    init render_basic

---
+++ render_basic/src/canvas.hxx
@@ -0,0 +1,101 @@
+//
+// Created by Алексей Крукович on 25.04.23.
+//
+
+#ifndef RENDER_BASIC_CANVAS_HXX
+#define RENDER_BASIC_CANVAS_HXX
+
+#include <compare>
+#include <cstdint>
+#include <fstream>
+#include <stdexcept>
+#include <string_view>
+#include <vector>
+
+namespace graphics {
+
+using namespace std::literals;
+
+struct Color
+{
+    std::uint8_t red{};
+    std::uint8_t green{};
+    std::uint8_t blue{};
+
+    auto operator<=>(const Color& color) const = default;
+};
+
+inline constexpr Color red{ 255, 0, 0 };
+inline constexpr Color green{ 0, 255, 0 };
+inline constexpr Color blue{ 0, 0, 255 };
+
+using Pixels = std::vector<Color>;
+
+static std::ofstream& operator<<(std::ofstream& out, const Pixels& pixels) {
+    out.write(reinterpret_cast<const char*>(pixels.data()),
+              pixels.size() * sizeof(Pixels::value_type));
+    return out;
+}
+
+static std::ifstream& operator>>(std::ifstream& in, Pixels& pixels) {
+    in.read(reinterpret_cast<char*>(pixels.data()),
+            pixels.size() * sizeof(Pixels::value_type));
+
+    return in;
+}
+
+class Canvas
+{
+private:
+    std::size_t m_width{};
+    std::size_t m_height{};
+    Pixels m_pixels{};
+
+public:
+    Canvas(std::size_t width, std::size_t height)
+        : m_width{ width }, m_height{ height }, m_pixels(m_width * m_height) {}
+
+    void saveImage(std::string_view filename) {
+        std::ofstream out{ filename.data(), std::ios::binary };
+        out.exceptions(std::ios::failbit);
+        out << "P6\n"sv << m_width << ' ' << m_height << '\n' << 255 << '\n';
+        out << m_pixels;
+    }
+
+    void loadImage(std::string_view filename) {
+        std::ifstream in{ filename.data(), std::ios::binary };
+        in.exceptions(std::ios::failbit);
+        std::string header{};
+        std::string colorFormat{};
+        char ws{};
+        in >> header >> m_width >> m_height >> colorFormat >> std::noskipws >>
+            ws;
+        if (!std::iswspace(ws))
+            throw std::runtime_error{
+                "Error : loadImage : bad load image from file"s
+            };
+
+        m_pixels.resize(m_width * m_height);
+        in >> m_pixels;
+    }
+
+    void setPixel(std::size_t x, std::size_t y, Color color) {
+        m_pixels.at(y * m_width + x) = color;
+    }
+
+    [[nodiscard]] Color getPixel(std::size_t x, std::size_t y) const noexcept {
+        return m_pixels.at(y * m_width + x);
+    }
+
+    [[nodiscard]] const Pixels& getPixels() const noexcept { return m_pixels; }
+    [[nodiscard]] std::size_t getWidth() const noexcept { return m_width; }
+    [[nodiscard]] std::size_t getHeight() const noexcept { return m_height; }
+
+    [[nodiscard]] auto begin() { return m_pixels.begin(); }
+    [[nodiscard]] auto end() { return m_pixels.end(); }
+
+    auto operator<=>(const Canvas& canvas) const = default;
+};
+} // namespace graphics
+
+#endif // RENDER_BASIC_CANVAS_HXX
