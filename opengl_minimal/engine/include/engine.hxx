e11f4fa51483b4f39204cc4fad53d5422ca933a7
AllPlas <proprikol987@gmail.com>
2023-05-07 14:49:31 +0300

    update shaders

--- opengl_minimal/engine/include/engine.hxx
+++ opengl_minimal/engine/include/engine.hxx
@@ -5,42 +5,56 @@
 #ifndef SDL_ENGINE_EXE_ENGINE_HXX
 #define SDL_ENGINE_EXE_ENGINE_HXX
 
-#include <iosfwd>
-#include <string_view>
-#include <string>
-#include <memory>
 #include <functional>
+#include <iosfwd>
+#include <memory>
+#include <string>
+#include <string_view>
 
 enum class Event
 {
-  up_pressed,
-  up_released,
-  down_pressed,
-  down_released,
-  left_pressed,
-  left_released,
-  right_pressed,
-  right_released,
-  space_pressed,
-  space_released,
-  return_pressed,
-  return_released,
-  lctrl_pressed,
-  lctrl_released,
-  turn_off,
+    up_pressed,
+    up_released,
+    down_pressed,
+    down_released,
+    left_pressed,
+    left_released,
+    right_pressed,
+    right_released,
+    space_pressed,
+    space_released,
+    return_pressed,
+    return_released,
+    lctrl_pressed,
+    lctrl_released,
+    turn_off,
 
-  max_events,
+    max_events,
 };
 
 std::ostream& operator<<(std::ostream& out, Event event);
 
+struct Vertex
+{
+    float x{};
+    float y{};
+    float z{};
+};
+
+struct Triangle
+{
+    std::array<Vertex, 3> vertices{};
+};
+
 class IEngine
 {
 public:
-  virtual ~IEngine() = default;
-  virtual std::string initialize([[maybe_unused]] std::string_view config) = 0;
-  virtual void uninitialize() = 0;
-  virtual bool readInput(Event& event) = 0;
+    virtual ~IEngine() = default;
+    virtual std::string initialize([[maybe_unused]] std::string_view config) = 0;
+    virtual void uninitialize() = 0;
+    virtual bool readInput(Event& event) = 0;
+    virtual void renderTriangle(const Triangle& triangle) = 0;
+    virtual void swapBuffers() = 0;
 };
 
 std::unique_ptr<IEngine, std::function<void(IEngine*)>> createEngine();
@@ -49,14 +63,14 @@ void destroyEngine(IEngine* e);
 class IGame
 {
 public:
-  virtual ~IGame() = default;
-  virtual void initialize() = 0;
-  virtual void onEvent(Event event) = 0;
-  virtual void update() = 0;
-  virtual void render() const = 0;
+    virtual ~IGame() = default;
+    virtual void initialize() = 0;
+    virtual void onEvent(Event event) = 0;
+    virtual void update() = 0;
+    virtual void render() const = 0;
 };
 
 extern "C" IGame* createGame(IEngine* engine);
 extern "C" void destroyGame(IGame* game);
 
-#endif  // SDL_ENGINE_EXE_ENGINE_HXX
+#endif // SDL_ENGINE_EXE_ENGINE_HXX
--- opengl_minimal/engine/src/engine.cxx
+++ opengl_minimal/engine/src/engine.cxx
@@ -5,12 +5,42 @@
 
 #include <SDL3/SDL.h>
 #include <filesystem>
+#include <fstream>
 #include <future>
 #include <iostream>
 #include <optional>
 #include <stdexcept>
+#include <cassert>
 #include <thread>
 #include <unordered_map>
+
+#include "glad/glad.h"
+
+#define OM_GL_CHECK()                                                                            \
+    {                                                                                            \
+        const GLenum err = glGetError();                                                         \
+        if (err != GL_NO_ERROR) {                                                                \
+            switch (err) {                                                                       \
+            case GL_INVALID_ENUM:                                                                \
+                std::cerr << "GL_INVALID_ENUM" << std::endl;                                     \
+                break;                                                                           \
+            case GL_INVALID_VALUE:                                                               \
+                std::cerr << "GL_INVALID_VALUE" << std::endl;                                    \
+                break;                                                                           \
+            case GL_INVALID_OPERATION:                                                           \
+                std::cerr << "GL_INVALID_OPERATION" << std::endl;                                \
+                break;                                                                           \
+            case GL_INVALID_FRAMEBUFFER_OPERATION:                                               \
+                std::cerr << "GL_INVALID_FRAMEBUFFER_OPERATION" << std::endl;                    \
+                break;                                                                           \
+            case GL_OUT_OF_MEMORY:                                                               \
+                std::cerr << "GL_OUT_OF_MEMORY" << std::endl;                                    \
+                break;                                                                           \
+            }                                                                                    \
+            std::cerr << __FILE__ << ':' << __LINE__ << '(' << __FUNCTION__ << ')' << std::endl; \
+            assert(false);                                                                       \
+        }                                                                                        \
+    }
 
 using namespace std::literals;
 namespace fs = std::filesystem;
@@ -69,18 +99,25 @@ class EngineImpl final : public IEngine
 {
 private:
     SDL_Window* m_window{};
+    SDL_GLContext m_glContext{};
+    GLuint m_programId{};
 
 public:
     EngineImpl() = default;
+    ~EngineImpl() override { uninitialize(); }
 
     std::string initialize(std::string_view config) override {
         initSDL();
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
         m_window = createWindow();
+        createGLContext();
+        createProgram();
         return "";
     }
 
     void uninitialize() override {
-        SDL_DestroyWindow(m_window);
+        if (m_glContext) SDL_GL_DeleteContext(m_glContext);
+        if (m_window) SDL_DestroyWindow(m_window);
         SDL_Quit();
     }
 
@@ -101,6 +138,20 @@ public:
         }
 
         return false;
+    }
+
+    void renderTriangle(const Triangle& triangle) override {
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), triangle.vertices.data());
+        glEnableVertexAttribArray(0);
+        glValidateProgram(m_programId);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+    void swapBuffers() override {
+        SDL_GL_SwapWindow(m_window);
+        glClearColor(0.3f, 0.3f, 1.f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 
 private:
@@ -126,6 +177,230 @@ private:
         SDL_DestroyWindow(window);
         SDL_Quit();
         throw std::runtime_error{ "Error : failed call SDL_CreateRenderer: "s + SDL_GetError() };
+    }
+
+    [[maybe_unused]] void createGLContext() {
+        const std::string_view platform{ SDL_GetPlatform() };
+
+        int gl_major_ver{ 3 };
+        int gl_minor_ver{ 2 };
+        int gl_context_profile{ SDL_GL_CONTEXT_PROFILE_ES };
+
+        if (platform == "macOS") {
+            gl_major_ver = 4;
+            gl_minor_ver = 1;
+            gl_context_profile = SDL_GL_CONTEXT_PROFILE_CORE;
+        }
+
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, gl_context_profile);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, gl_major_ver);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, gl_minor_ver);
+
+        m_glContext = SDL_GL_CreateContext(m_window);
+        if (m_glContext == nullptr)
+            throw std::runtime_error{ "Error : createGLContext : bad create gl context"s };
+
+        auto load_gl_pointer = [](const char* functionName) {
+            auto functionPointer{ SDL_GL_GetProcAddress(functionName) };
+            return reinterpret_cast<void*>(functionPointer);
+        };
+
+        if (gladLoadGLES2Loader(load_gl_pointer) == 0)
+            throw std::runtime_error{ "Error : createGLContext : bad gladLoad"s };
+    }
+
+    void createProgram() {
+//        m_programId = glCreateProgram();
+//        createVertexShader();
+//        createFragmentShader();
+//
+//        glLinkProgram(m_programId);
+//        glBindAttribLocation(m_programId, 0, "a_position");
+//
+//        glUseProgram(m_programId);
+//        glEnable(GL_DEPTH_TEST);
+
+        GLuint vert_shader = glCreateShader(GL_VERTEX_SHADER);
+        OM_GL_CHECK()
+        std::string_view vertex_shader_src = R"(
+                                    #version 410 core
+                                    in vec3 a_position;
+                                    out vec4 v_position;
+
+                                    void main()
+                                    {
+                                        v_position = vec4(a_position, 1.0);
+                                        gl_Position = v_position;
+                                    }
+                                    )";
+        const char* source            = vertex_shader_src.data();
+        glShaderSource(vert_shader, 1, &source, nullptr);
+        OM_GL_CHECK()
+
+        glCompileShader(vert_shader);
+        OM_GL_CHECK()
+
+        GLint compiled_status = 0;
+        glGetShaderiv(vert_shader, GL_COMPILE_STATUS, &compiled_status);
+        OM_GL_CHECK()
+        if (compiled_status == 0)
+        {
+            GLint info_len = 0;
+            glGetShaderiv(vert_shader, GL_INFO_LOG_LENGTH, &info_len);
+            OM_GL_CHECK()
+            std::vector<char> info_chars(static_cast<size_t>(info_len));
+            glGetShaderInfoLog(vert_shader, info_len, nullptr, info_chars.data());
+            OM_GL_CHECK()
+            glDeleteShader(vert_shader);
+            OM_GL_CHECK()
+
+        }
+
+        // create fragment shader
+
+        GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+        OM_GL_CHECK()
+        std::string_view fragment_shader_src = R"(
+                      #version 410 core
+                      precision mediump float;
+
+                      in vec4 v_position;
+
+                      out vec4 frag_color;
+
+                      // try main_one function name on linux mesa drivers
+                      void main()
+                      {
+                          if (v_position.z >= 0.0)
+                          {
+                              float light_green = 0.5 + v_position.z / 2.0;
+                              frag_color = vec4(0.0, light_green, 0.0, 1.0);
+                          } else
+                          {
+                              float color = 0.5 - (v_position.z / -2.0);
+                              frag_color = vec4(color, 0.0, 0.0, 1.0);
+                          }
+                      }
+                      )";
+        source                          = fragment_shader_src.data();
+        glShaderSource(fragment_shader, 1, &source, nullptr);
+        OM_GL_CHECK()
+
+        glCompileShader(fragment_shader);
+        OM_GL_CHECK()
+
+        compiled_status = 0;
+        glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &compiled_status);
+        OM_GL_CHECK()
+        if (compiled_status == 0)
+        {
+            GLint info_len = 0;
+            glGetShaderiv(fragment_shader, GL_INFO_LOG_LENGTH, &info_len);
+            OM_GL_CHECK()
+            std::vector<char> info_chars(static_cast<size_t>(info_len));
+            glGetShaderInfoLog(
+                fragment_shader, info_len, nullptr, info_chars.data());
+            OM_GL_CHECK()
+            glDeleteShader(fragment_shader);
+            OM_GL_CHECK()
+        }
+
+        // now create program and attach vertex and fragment shaders
+
+        m_programId = glCreateProgram();
+        OM_GL_CHECK()
+        if (0 == m_programId)
+        {
+        }
+
+        glAttachShader(m_programId, vert_shader);
+        OM_GL_CHECK()
+        glAttachShader(m_programId, fragment_shader);
+        OM_GL_CHECK()
+
+        // bind attribute location
+        glBindAttribLocation(m_programId, 0, "a_position");
+        OM_GL_CHECK()
+        // link program after binding attribute locations
+        glLinkProgram(m_programId);
+        OM_GL_CHECK()
+        // Check the link status
+        GLint linked_status = 0;
+        glGetProgramiv(m_programId, GL_LINK_STATUS, &linked_status);
+        OM_GL_CHECK()
+        if (linked_status == 0)
+        {
+            GLint infoLen = 0;
+            glGetProgramiv(m_programId, GL_INFO_LOG_LENGTH, &infoLen);
+            OM_GL_CHECK()
+            std::vector<char> infoLog(static_cast<size_t>(infoLen));
+            glGetProgramInfoLog(m_programId, infoLen, nullptr, infoLog.data());
+            OM_GL_CHECK()
+        }
+
+        // turn on rendering with just created shader program
+        glUseProgram(m_programId);
+        OM_GL_CHECK()
+
+        glEnable(GL_DEPTH_TEST);
+        // glDisable(GL_DEPTH_TEST);
+
+    }
+
+    void createVertexShader() {
+        GLuint vertexShader{ glCreateShader(GL_VERTEX_SHADER) };
+        std::string shaderSource{ readFile(
+            "/Users/aleksey/lesta-course/opengl_minimal/vertex_shader.glsl") };
+        std::cout << shaderSource.c_str() << '\n';
+        auto source{ shaderSource.c_str() };
+        glShaderSource(vertexShader, 1, &source, nullptr);
+
+        glCompileShader(vertexShader);
+
+        GLint compileStatus{};
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compileStatus);
+        if (compileStatus == 0) {
+            glDeleteShader(vertexShader);
+            throw std::runtime_error{ "Error : createVertexShader : bad compile shader"s };
+        }
+
+        glAttachShader(m_programId, vertexShader);
+    };
+
+    void createFragmentShader() {
+        GLuint fragmentShader{ glCreateShader(GL_FRAGMENT_SHADER) };
+        std::string shaderSource{ readFile(
+            "/Users/aleksey/lesta-course/opengl_minimal/fragment_shader.glsl"s) };
+
+        auto source{ shaderSource.c_str() };
+        glShaderSource(fragmentShader, 1, &source, nullptr);
+
+        glCompileShader(fragmentShader);
+
+        GLint compileStatus{};
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &compileStatus);
+        if (compileStatus == 0) {
+            glDeleteShader(fragmentShader);
+            throw std::runtime_error{ "Error : createFragmentShader : bad compile shader"s };
+        }
+
+        glAttachShader(m_programId, fragmentShader);
+    }
+
+    static std::string readFile(const fs::path& path) {
+        std::ifstream in{ path };
+        if (!in.is_open()) throw std::runtime_error{ "Error : readFile : bad open file"s };
+
+        std::string result{};
+        while (in) {
+            std::string buf{};
+            std::getline(in, buf, '\n');
+            if (!in) break;
+            result += buf;
+            result += '\n';
+        }
+
+        return result;
     }
 };
 
@@ -192,7 +467,8 @@ reloadGame(std::unique_ptr<IGame, std::function<void(IGame* game)>> oldGame,
 int main() {
     try {
         auto engine{ createEngine() };
-        engine->initialize("");
+        auto answer = engine->initialize("");
+        if (!answer.empty()) { return EXIT_FAILURE; }
         std::cout << "start app"sv << std::endl;
 
         std::string_view libraryName{ SDL_GetPlatform() == "Windows"s ? "game.dll" : "./game.so" };
@@ -253,6 +529,11 @@ int main() {
                 game->render();
                 updateResult = std::async(std::launch::async, &IGame::update, game.get());
             }
+
+            Triangle t{ { -0.5, -0.5, 0.5, 0.5, -0.5, 0.5, 0.0, 0.8, -0.5 } };
+
+            engine->renderTriangle(t);
+            engine->swapBuffers();
         }
 
         engine->uninitialize();
