59b0121512d07019cacda8626f4ea1db3af0e520
AllPlas <proprikol987@gmail.com>
2023-06-18 23:09:52 +0300

    minor fix

--- prepare_engine_to_game/engine/src/view.cxx
+++ prepare_engine_to_game/engine/src/view.cxx
@@ -5,8 +5,8 @@
 glm::mat3 View::getViewMatrix() const {
     glm::mat3 view{ 1.0f };
     //  view = glm::translate(view, glm::vec3(-m_x, -m_y, 0.0f));
-    view[2][0] = -m_x;
-    view[2][1] = -m_y;
+    view[2][0] = -m_x * m_scale;
+    view[2][1] = -m_y * m_scale;
     view[0][0] = m_scale;
     view[1][1] = m_scale;
     // view = glm::scale(view, glm::vec3(m_scale, m_scale, 1.0f));
