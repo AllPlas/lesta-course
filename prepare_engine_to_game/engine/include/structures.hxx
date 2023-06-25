7dead555bfbe7519b49534750da774d1b6bac78b
AllPlas <proprikol987@gmail.com>
2023-06-26 02:31:11 +0300

    added operator<=>

--- prepare_engine_to_game/engine/include/structures.hxx
+++ prepare_engine_to_game/engine/include/structures.hxx
@@ -2,6 +2,7 @@
 #define ENGINE_PREPARE_TO_GAME_STRUCTURES_HXX
 
 #include <cmath>
+#include <compare>
 #include <numbers>
 #include <optional>
 
@@ -9,18 +10,24 @@ struct Position
 {
     float x{};
     float y{};
+
+    bool operator<=>(const Position& position) const = default;
 };
 
 struct Size
 {
     float width{};
     float height{};
+
+    bool operator<=>(const Size& size) const = default;
 };
 
 struct Scale
 {
     float x{ 1.0f };
     float y{ 1.0f };
+
+    bool operator<=>(const Scale& scale) const = default;
 };
 
 class Angle
@@ -36,6 +43,8 @@ public:
 
     [[nodiscard]] float getInRadians() const noexcept;
     [[nodiscard]] float getInDegrees() const noexcept;
+
+    bool operator<=>(const Angle& angle) const = default;
 };
 
 struct LineSegment
@@ -50,7 +59,7 @@ struct Rectangle
     Position xy{};
     Size wh{};
 
-   [[nodiscard]] bool contains(Position position) const noexcept;
+    [[nodiscard]] bool contains(Position position) const noexcept;
 };
 
 std::optional<LineSegment> intersect(LineSegment l1, LineSegment l2);
